2fc73c55169163c391e845590075d1a114a69568
DavidBorts <Davidaborts@gmail.com>
2023-01-06 11:31:48 -0500

    Added function to merge checkpoints + cleaned up

--- npy_convert.py
+++ npy_convert.py
@@ -1,5 +1,9 @@
 import numpy as np
 import sys
+import os
+
+# Local files
+import Darktable_constants as c
 
 def convert(vals, output_file_path):
 
@@ -11,13 +15,29 @@ def convert(vals, output_file_path):
     with open(output_file_path, 'wb') as f:
         np.save(f, vals)
 
+def merge(param_files, params_path, output_file_path):
+
+    # Loading in all of the .npy files as
+    # ndarrays in a list
+    param_arrays = []
+    for param_file in param_files:
+        param_array = np.load(os.path.join(params_path, param_file))
+        param_arrays.append(param_array)
+    
+    # Concatenating all of the param arrays along the
+    # horizontal axis
+    merged_params = np.hstack(param_arrays)
+
+    with open(output_file_path, 'wb') as f:
+        np.save(f, merged_params)
+
 if __name__ == '__main__':
 
     # Command-line arguments
     params_file = sys.argv[1]
     output_file_path = sys.argv[2]
 
-    # Array to store all the para, values in the file
+    # Array to store all the param values in the file
     vals = []
 
     # Reading params from file and appending to vals[]
