8ae110e2db53ef7bd7148caf34384f15f28e5c4c
DavidBorts <Davidaborts@gmail.com>
2023-01-06 01:51:10 -0500

    Cleaned up sweep helper script

--- Darktable_sweep.py
+++ Darktable_sweep.py
@@ -6,11 +6,11 @@
 # Sample ffmpeg command: ffmpeg -r 25 -f image2 -s 1920x1080 -i [image name]_[proxy type]_[parameter]_sweep_%04d.png -vcodec libx264 -crf 15  -pix_fmt yuv420p [VIDEO NAME].mp4
 #
 # How to use: python Darktable_sweep.py [proxy type]_[parameter] [path to .DNG image to use as input] [integer number of values to sweep over]
+#TODO: Support multiple params per proxy
 
 import sys
 import os
 import time
-import shutil
 import numpy as np
 import torch
 import torch.nn as nn
@@ -27,13 +27,13 @@ from npy_convert import convert
 add_params = True
 skip_connect = True
 clip_output = True
-use_checkpoint = True
 learning_rate = 0.0001
 gamma = 0.1
 step_size = 7
-input_dir = c.SWEEP_INPUT_DIR
-output_dir = c.SWEEP_OUTPUT_DIR
 
+'''
+Sweeping across the parameter ranges
+'''
 def sweep(proxy_type, param, possible_values, num):
     min = possible_values[0][0]
     max = possible_values[0][1]
@@ -44,64 +44,56 @@ def sweep(proxy_type, param, possible_values, num):
     convert(vals, vals_path)
         
     return vals, vals_path
-        
-def copy_img(img_path, input_dir, vals, render_first=False):
-    
-    # Getting the name of the image
-    image = img_path.split('\\')[-1]
-    print(f'image: {image}')
-    
-    # Tracking the first input to avoid rendering the same image twice
-    first_input = None
-    
-    # Extracting necessary params from the source image
-    raw_prepare_params, temperature_params = dt.read_dng_params(img_path)
-    
-    for val in vals[0]:
-    
-        # Getting path of the corresponding input image
-        input_path = os.path.join(input_dir, f'{image}_{proxy_type}_{param}')
-        input_path = (repr(input_path).replace('\\\\', '/')).strip("'") + f'_{val}.tif' # Dealing with Darktable CLI pickiness
 
-        # Making sure that the same input image is not generated multiple times
-        if first_input is None:
-            first_input = input_path # Path of the original input image to be copied later
-
-            # Assembling a dictionary of all of the original params for the source image
-            # (used to render proxy input)
-            if render_first:
-                original_params = dt.get_params_dict(None, None, None, temperature_params, raw_prepare_params)
-
-                # Rendering an unchanged copy of the source image for model input
-                dt.render(img_path, input_path, original_params)
-        else:
-            _ = shutil.copyfile(first_input, input_path) # Copying the first input image to get the others
-                
-    print('Input data copied.')
-
+'''
+Procedure to set up data loading
+'''
 def dataloader(proxy_type, param, input_path, vals_path):
-    # Set up data loading.
+
     since = time.time()
-    image_dataset = Darktable_Dataset(root_dir = c.IMAGE_ROOT_DIR, stage=1, proxy_type=proxy_type, param=param, input_dir=input_path, params_file=vals_path,sweep=True)
-    data = torch.utils.data.DataLoader(image_dataset, 
-                                               batch_size=c.PROXY_MODEL_BATCH_SIZE, 
-                                               sampler=image_dataset.sweep_sampler,
-                                               num_workers=1)#num_workers should be 4
+
+    # Create dataset
+    image_dataset = Darktable_Dataset(
+        root_dir=c.IMAGE_ROOT_DIR, 
+        stage=1, 
+        proxy_type=proxy_type, 
+        param=param, 
+        input_dir=input_path, 
+        params_file=vals_path, 
+        sweep=True
+        )
+
+    # Wrap dataset with a DataLoader
+    #TODO: remove dataloader?
+    data = torch.utils.data.DataLoader(
+        image_dataset, 
+        batch_size=c.PROXY_MODEL_BATCH_SIZE, 
+        sampler=image_dataset.sweep_sampler,
+        num_workers=1
+        )
+
     time_elapsed = time.time() - since
     print('Data Loade prepared in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
     sys.stdout.flush()
+
     return data
 
+'''
+Procedure to initialize the proxy and load in model weights.
+'''
 def model_init(model_out_dir, possible_params):
     
     # Set up model.
-    unet = UNet(num_input_channels=c.NUM_IMAGE_CHANNEL + len(possible_params),
-                num_output_channels=c.NUM_IMAGE_CHANNEL, skip_connect=skip_connect, add_params=add_params, clip_output=clip_output)
-    if use_checkpoint:
-        start_epoch = load_checkpoint(unet, model_out_dir) #weight_out_dir
-        lr = learning_rate * (gamma ** (start_epoch//step_size))
-    else:
-        start_epoch = 0 
+    unet = UNet(
+        num_input_channels=c.NUM_IMAGE_CHANNEL + len(possible_params),
+        num_output_channels=c.NUM_IMAGE_CHANNEL, 
+        skip_connect=skip_connect, 
+        add_params=add_params, 
+        clip_output=clip_output
+        )
+
+    start_epoch = load_checkpoint(unet, model_out_dir) #weight_out_dir
+    
     if use_gpu:
         unet.cuda()
     if torch.cuda.device_count() > 1:
@@ -116,7 +108,7 @@ def model_init(model_out_dir, possible_params):
 
 if __name__ == '__main__':
     proxy_type, param = sys.argv[1].split('_') # Type of proxy to evaluate [module, parameter]
-    img_path = sys.argv[2]                     #  Path of the image to use as input
+    img_path = sys.argv[2]                     # Path of the image to use as input
     num = int(sys.argv[3])                     # Number of parameter values to sweep over
 
     # Constants
@@ -124,27 +116,22 @@ if __name__ == '__main__':
     model_out_dir = os.path.join(c.IMAGE_ROOT_DIR, c.STAGE_1_PATH, proxy_type + '_' + param + '_' + c.MODEL_WEIGHTS_PATH)
     use_gpu = torch.cuda.is_available()
 
-    # Getting the paths for model inputs and outputs
-    input_path = os.path.join(c.IMAGE_ROOT_DIR, c.STAGE_1_PATH, proxy_type + '_' + param + '_' + input_dir)
-    output_path = os.path.join(c.IMAGE_ROOT_DIR, c.STAGE_1_PATH, proxy_type + '_' + param + '_' + output_dir)
-    if not os.path.exists(input_path):
-        os.mkdir(input_path)
+    # Getting the path at which to save model outputs
+    output_path = os.path.join(c.IMAGE_ROOT_DIR, c.STAGE_1_PATH, proxy_type + '_' + param + '_' + c.SWEEP_OUTPUT_DIR)
     if not os.path.exists(output_path):
         os.mkdir(output_path)
+        print('Created directory for sweep outputs at: \n' + output_path)
     
     # Sweeping over the param range and writing to a .npy file
     vals, vals_path = sweep(proxy_type, param, possible_params, num)
     
-    # Copying the image at img_path num times to create the input dataset
-    copy_img(img_path, input_path, vals)
-    
     # Setting up dataloader
-    data = dataloader(proxy_type, param, input_path, vals_path)
+    data = dataloader(proxy_type, param, img_path, vals_path)
 
     # Initializing the model
     model, criterion, optimizer = model_init(model_out_dir, possible_params)
     
-    # Sweeping
+    # Sweeping the proxy
     eval(
         model, 
         data, 
