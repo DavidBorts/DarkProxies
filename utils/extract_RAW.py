0ce3d2f78217c654e5f868bc5be7fda7103b0ecb
DavidBorts <Davidaborts@gmail.com>
2023-03-21 01:00:25 -0400

    added helper script to print CFAs

--- utils/extract_RAW.py
+++ utils/extract_RAW.py
@@ -1,5 +1,7 @@
 # Various helper scripts to extract RAW file metadata using RawPy
 
+import sys
+import os
 import rawpy
 
 # Code adapted from: https://stackoverflow.com/a/67280050/20394190
@@ -8,3 +10,13 @@ def get_cfa(raw_file_path):
 
     cfa = "".join([chr(raw.color_desc[i]) for i in raw.raw_pattern.flatten()])
     return cfa
+
+if __name__ == "__main__":
+    dng_dir = sys.argv[1]
+
+    dng_images = os.listdir(dng_dir)
+
+    print("Getting cfa's:")
+    for img in dng_images:
+        print("image name: " + img)
+        print(get_cfa(os.path.join(dng_dir, img)))
