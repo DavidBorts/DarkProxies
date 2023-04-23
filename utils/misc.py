bff5ffdb00945d311e2687ee4203e8857877d98b
DavidBorts <Davidaborts@gmail.com>
2023-04-22 22:46:02 -0400

    renamed lhs.py

---
+++ utils/misc.py
@@ -0,0 +1,15 @@
+# Miscellaneous util functions
+
+import Constants as c
+
+def get_possible_values(proxy_type, params):
+    '''
+    Given a proxy type and a subset of its input params,
+    return the corresponding ranges of possible values
+    of those parameters
+    '''
+    all_possible_values = c.POSSIBLE_VALUES[proxy_type]
+    all_param_names = c.PARAM_NAMES[proxy_type]
+
+    possible_values = [all_possible_values[all_param_names.index(param)] for param in params]
+    return possible_values
