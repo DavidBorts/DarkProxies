504328725951d3afc7a6f2a933e28eed59149da9
DavidBorts <Davidaborts@gmail.com>
2023-05-06 15:56:11 -0400

    bugfix

--- Models.py
+++ Models.py
@@ -185,8 +185,7 @@ class UNet(nn.Module):
                     param_sixteenth = torch.reshape(param, self.params_size)
                     param = torch.tile(param_sixteenth, (16,16))
                 else: # embedding type is "linear_to_value"
-                    print("shapes: " + str(self.params_size) + ' ' + str(param.shape))
-                    param = torch.ones(self.params_size) * param
+                    param = torch.ones(self.params_size) * param[:,:,None,None]
                 x = torch.cat((x, param), dim=1)
             else:
                 param = x[:,3:,:,:]
